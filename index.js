fa7a079fdf5485a878ca0437caec9397a6204139
Austin <baik.austin@gmail.com>
2022-04-01 13:25:51 -0700

    removed commented out code

--- index.js
+++ index.js
@@ -8,26 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
-// const apiUrl = 'https://api.shrtco.de/v2/shorten?url='
-
-    // function shortenUrl(url){
-    //     let urlforApi = `${apiUrl}${url}`
-    //     // https://api.shrtco.de/v2/shorten?url=
-    //     // https://api-ssl.bitly.com/v4/shorten
-    //      fetch("urlforApi")
-    //     // //  {
-    //     //     //  method: "POST",
-    //     //      {'headers': 
-    //     //         'Access-Control-Allow-Origin' : '*'})
-    //     //         // 'Authorization': 'Bearer 3c471cf3f50af5250e7dc5214241f5329461b1fb',
-    //     //         // 'Content-Type': 'application/JSON'
-    //     //     //  },
-    //     //     //  "long_url": "url"
-    //     // //  })
-    //      .then((resp) => resp.json())
-    //      .then((json) => console.log(json))
-    //      }
-
 function shortenUrl(url) {
         // let urlforApi = `${apiUrl}${url}`
         fetch(`https://api.shrtco.de/v2/shorten?url=${url}`) //API call is initiated to the API url that is interpolated with the user submitted url
@@ -66,49 +46,10 @@ function addUrlToDom(infoJson) {
     document.getElementById("created_urls").appendChild(card)
 
     console.log('card', card)
-    //qq query selector? 
-     card.querySelector('#close').addEventListener('click', () => {
+
+    card.querySelector('#close').addEventListener('click', () => {
         card.remove()
 
     })
     
-    // card.querySelector('#close').addEventListener('click', () => {
-    //     card.innerHTML = ''
-    // })
-
 }
-
-
-
-
-
-// function addShortUrlToDom(infoJson) {
-//     let shortLink = infoJson.result.full_short_link
-//     let aShort = document.createElement('a')
-//     document.getElementById("url_list").appendChild(aShort)
-//     // document.getElementById("created_urls").a.innerHTML = "shortLink"
-//     // a.addEventListener
-//     aShort.text = shortLink
-//     aShort.setAttribute('href', shortLink)
-// }
-
-// function addOrgUrlToDom(infoJson) {
-//     let orgLink = infoJson.result.original_link
-//     let aLong = document.createElement('a')
-//     document.getElementById("url_list").appendChild(aLong)
-//     // document.getElementById("created_urls").a.innerHTML = "shortLink"
-//     // a.addEventListener
-//     aLong.text = orgLink
-//     aLong.setAttribute('href', aLong)
-// }
-
-
-
-
-// function deleteUrl() {
-// addEventListener
-// }
-
-//make cards! - add each url to a card ; 
-//build in event listener to card
-//be able to delete the whole card 
